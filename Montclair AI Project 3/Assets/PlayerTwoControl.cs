a2c94a59ec50663dc4677cdefee355ec63b039b5
Buffet Time <radd86@gmail.com>
2018-02-26 16:15:20 -0500

    added movement and jumping to each player (rough)

---
+++ Montclair AI Project 3/Assets/PlayerTwoControl.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using UnityEngine;
+[RequireComponent(typeof(Rigidbody))]
+
+public class PlayerTwoControl : MonoBehaviour 
+{
+	public float horizontalSpeed;	// higher the number the slower you move
+	public float jumpSpeed; 
+	private Rigidbody2D rigidBody;	// reference to the rigidBody
+	public Vector3 jump;
+	public float jumpForce = 2.0f;
+	public bool isGrounded = true;
+
+	/// <summary>
+	/// Intialization
+	/// </summary>
+	void Start()
+	{
+		rigidBody = GetComponent<Rigidbody2D>();
+        jump = new Vector3(0.0f, 2.0f, 0.0f);
+	}
+	
+	/// <summary>
+	/// Fixeds the update.
+	/// </summary>
+	void FixedUpdate()
+	{
+		if (transform.position.y < -2.6)
+		{
+			isGrounded = true;
+		}
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+			rigidBody.AddForce(transform.right * -horizontalSpeed);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+			rigidBody.AddForce(transform.right * horizontalSpeed);
+        }
+		if (Input.GetKeyDown(KeyCode.RightControl) && isGrounded)
+		{
+			rigidBody.AddForce(jump * jumpForce, ForceMode2D.Impulse);
+			isGrounded = false;
+		}
+	}
+}
